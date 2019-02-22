bc72bf7bf52cd9616d0da44f5e0294b80557c22f
Gabriel <gabriel.guimaraes@prof.infnet.edu.br>
2019-02-21 21:43:35 -0300

    Correção de sombra no celular

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ $('#vendas_collapse').on('hidden.bs.collapse', function () {
 $(window).on("scroll", function(){
 	var distanciaDoTopo = $(window).scrollTop();
 
-	if (distanciaDoTopo > 400) {
-		distanciaDoTopo = 400;
+	if (distanciaDoTopo > 300) {
+		distanciaDoTopo = 300;
 	}
 
 	$("#header_shadow").css("top", (distanciaDoTopo / 15) + "px");
