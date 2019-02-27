8baf3a14ec6a33006c549ada5a51cfa25a03ae42
Gabriel <gabriel.guimaraes@prof.infnet.edu.br>
2019-02-26 21:11:54 -0300

    Inclusão do ScrollReveal

--- js/main.js
+++ js/main.js
@@ -23,3 +23,15 @@ $(window).on("scroll", function(){
 
 	$("#header_shadow").css("top", (distanciaDoTopo / 10) + "px");
 })
+
+var opcoesSR = {
+	delay: 300,
+	duration: 1000,
+	origin: 'bottom',
+	distance: '50px',
+	viewOffset: {
+        bottom: 300
+    }
+}
+ScrollReveal().reveal('#clientes-box', opcoesSR)
+ScrollReveal().reveal('.feature', opcoesSR)
